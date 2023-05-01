dc050fa80086f75672e2b001e1bc5ea1dd387b03
ali-khoshqamat <ali_khoshghamat@yahoo.com>
2023-05-01 13:12:28 +0330

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/NobaanPWI-Task/sw.js', { scope: '/NobaanPWI-Task/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
